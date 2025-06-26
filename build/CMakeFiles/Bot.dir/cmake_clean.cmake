9035c358318ce1957c13ebe8ef672bdd30adddbb
Arturhalk <komissarov.artur2009@gmail.com>
2025-06-26 21:19:08 +0300

    first commit

---
+++ build/CMakeFiles/Bot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Bot"
+  "Bot.pdb"
+  "CMakeFiles/Bot.dir/main.cc.o"
+  "CMakeFiles/Bot.dir/main.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
