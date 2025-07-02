70d9d51400ab6bdfbe6f19b70d4c9584161d6ad5
Arturhalk <komissarov.artur2009@gmail.com>
2025-07-02 18:02:10 +0300

    Add function RecordingPictures and divided everything into functions

---
+++ src/Bot.h
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <tgbot/tgbot.h>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <pqxx/pqxx>
+#include <filesystem>
+#include <vector>
+/*Основная концепция проекта
+Когда пользователь нажимает /start, то программа проверяет по базе данных PostgreSQL,
+какое количество времени прошло с последнией отправки изображения(если прошло меньше 24 часов,
+то программа пишет сообщение "Ещё не прошло 24 часа, попробуйте через(количество оставшегося времени)",
+иначе отправляет изображение с животным и названием этого животного)*/
+
+/*PostgreSQL
+В таблице будем хранить id, previous photo и hours. По id  будем искать пользователя,
+затем будем проверять прошло ли 24 часа, с момента отправки последнего фото с животным,
+а затем искать изображение в локальной папке так, чтобы оно не повторялось с прошлым отосланым изображением*/
+
+std::filesystem::path RecordingPictures()
+{
+    std::vector<std::filesystem::path> pictures;
+    try
+    {
+        std::string path = "../pictures_with_animals";
+
+        for (const auto &entry : std::filesystem::directory_iterator(path))
+            pictures.push_back(entry.path());
+        // for (const auto &filePath : pictures)
+        // {
+        //     std::cout << filePath << std::endl;
+        // }
+    }
+    catch (std::exception const &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return pictures[1];
+}
+
+std::filesystem::path RandomPictureSelection(std::vector<std::filesystem::path> &pictures) {}
+
+void BotTG()
+{
+    TgBot::Bot bot("7779372699:AAEmijaJMUUgD7xcaXC0VSZqpUck9XDnhzU");
+
+    pqxx::connection C("dbname = telegram user = postgres password = postgres hostaddr = 127.0.0.1 port = 5432");
+
+    if (C.is_open())
+    {
+        std::cout << "Успешное подключение к " << C.dbname() << std::endl;
+    }
+    else
+    {
+        std::cout << "Не удалось подключиться к базе данных" << std::endl;
+        return;
+    }
+
+    bot.getEvents().onCommand("start", [&bot, &C](TgBot::Message::Ptr message)
+                              {
+                                  //bot.getApi().sendPhoto(message->chat->id, TgBot::InputFile::fromFile(RecordingPictures(), RecordingPictures().filename()));
+                                  auto user_id = message->chat->id;
+                                      try
+                                      {
+                                          pqxx::work worker(C);
+
+                                          pqxx::result res = worker.exec_params("SELECT id FROM info WHERE id = $1", user_id);
+
+                                          if(res.empty()){
+
+                                          } else {
+                                              auto picture_and_date = worker.exec_params("SELECT previous_photo,last_sending FROM info WHERE id = $1",user_id);
+                                          }
+                                          worker.commit();
+                                      }
+                                      catch (std::exception const &e)
+                                      {
+                                          std::cout << e.what() << std::endl;
+                              });
+
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
+                                 {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {return;} });
+    try
+    {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true)
+        {
+            printf("Long poll started\n");
+            longPoll.start();
+            printf("Long poll ended\n");
+        }
+        }
+        catch (TgBot::TgException &e)
+        {
+            printf("error: %s\n", e.what());
+        }
+}
--- src/main.cc
+++ src/main.cc
@@ -1,96 +1,11 @@
-#include <stdio.h>
-#include <tgbot/tgbot.h>
-#include <chrono>
-#include <thread>
 #include <iostream>
-#include <pqxx/pqxx>
-// int main()
-// {
-//     TgBot::Bot bot("7779372699:AAEmijaJMUUgD7xcaXC0VSZqpUck9XDnhzU");
-//     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message)
-//                               {
-//                                 auto tmp = bot.getApi().sendMessage(message->chat->id, "Hi!");
-//                                 std::chrono::seconds time(3); // or whatever
-//                                 std::this_thread::sleep_for(time); });
-//     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
-//                                  {
-//         printf("User wrote %s\n", message->text.c_str());
-//         if (StringTools::startsWith(message->text, "/start")) {return;}
-//         bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text); });
-//     try
-//     {
-//         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-//         TgBot::TgLongPoll longPoll(bot);
-//         while (true)
-//         {
-//             printf("Long poll started\n");
-//             longPoll.start();
-//         }
-//     }
-//     catch (TgBot::TgException &e)
-//     {
-//         printf("error: %s\n", e.what());
-//     }
-//     return 0;
-// }
+#include "Bot.h"
+#include <filesystem>
+
+using namespace std;
+
 int main()
 {
-    TgBot::Bot bot("7779372699:AAEmijaJMUUgD7xcaXC0VSZqpUck9XDnhzU");
-    // try{
-    //     pqxx::connection C("dbname = telegram user = postgres password = postgres hostaddr = 127.0.0.1 port = 5432");
-    //     pqxx::work worker(C);
-    //     pqxx::result response = worker.exec("SELECT * FROM info");
-    //     for (size_t i = 0; i < response.size(); i++)
-    //     {
-    //         std::cout << "Id: " << response[i][0] << std::endl;
-    //     }
-    // } catch(std::exception const &e){
-    //     std::cout << e.what() <<std::endl;
-    // }
-    auto timespan = std::chrono::hours(24);
-    bot.getEvents().onCommand("start", [&bot, &timespan](TgBot::Message::Ptr message)
-                              {
-
-                                  bot.getApi().sendMessage(message->chat->id,"Сегодня вы:");
-                                  bot.getApi().sendPhoto(message->chat->id, "");
-                                });
-    bot.getEvents().onAnyMessage([&bot, &timespan](TgBot::Message::Ptr message)
-                                 {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {return;}
-        if(timespan.count() == 0)
-        {
-            bot.getApi().sendMessage(message->chat->id, "Сегодня вы:");
-            //bot.getApi().sendPhoto(message->chat->id, "");
-        }
-        else
-        {
-            bot.getApi().sendMessage(message->chat->id, "Пока ещё 24 часа не прошло");
-        } });
-    try
-    {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
-        while (true)
-        {
-            printf("Long poll started\n");
-            longPoll.start();
-            printf("Long poll ended\n");
-        }
-    }
-    catch (TgBot::TgException &e)
-    {
-        printf("error: %s\n", e.what());
-    }
+    BotTG();
     return 0;
 }
-/*Основная концепция проекта
-Когда пользователь нажимает /start, то программа проверяет по базе данных PostgreSQL,
-какое количество времени прошло с последнией отправки изображения(если прошло меньше 24 часов,
-то программа пишет сообщение "Ещё не прошло 24 часа, попробуйте через(количество оставшегося времени)",
-иначе отправляет изображение с животным и названием этого животного)*/
-
-/*PostgreSQL
-В таблице будем хранить id, previous photo и hours. По id  будем искать пользователя,
-затем будем проверять прошло ли 24 часа, с момента отправки последнего фото с животным,
-а затем искать изображение в локальной папке так, чтобы оно не повторялось с прошлым отосланым изображением*/
